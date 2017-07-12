0469b3528d59c73426e22a116270a1a684ef5117
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-07-12 11:22:03 -0500

    made the thing do the thing

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace family_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You can do it, girl!");
+
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){ 
+                {"name", "Amy"},
+                {"age", "30"}
+            });
+            myFamily.Add("mother", new Dictionary<string, string>(){ 
+                {"name", "Julie"},
+                {"age", "59"}
+            });
+            myFamily.Add("father", new Dictionary<string, string>(){ 
+                {"name", "Newell"},
+                {"age", "55"}
+            });
+            myFamily.Add("brother", new Dictionary<string, string>(){ 
+                {"name", "Wesley"},
+                {"age", "28"}
+            });
+
+            foreach( KeyValuePair<string, Dictionary<string, string>> kvp in myFamily )
+            {
+                Console.WriteLine($"{kvp.Value["name"]} is my {kvp.Key} and is {kvp.Value["age"]} years old.");
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # csharp-family-dictionary
+
+NSS Exercise
+
+## Instructions
+
+Define a Dictionary that contains information about several members of your family. Use the following example as a template.
+
+```csharp
+Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+myFamily.Add("sister", new Dictionary<string, string>(){ 
+    {"name", "Krista"},
+    {"age", "42"}
+});
+```
+Next, iterate over each item in myFamily and produce the following output.
+
+```
+Krista is my sister and is 42 years old
+```
